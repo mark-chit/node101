c0e10ee5e24820747b68fa528af05544ecfe8272
Mark Chittenden <41925735+mark-chit@users.noreply.github.com>
2024-02-18 23:39:27 +0000

    Update utility.js

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -16,6 +16,22 @@ export function add(a, b) {
 }
 
 export function compare(arr, arr2) {
-  return arr.length === arr2.length;
+  if (arr.length !== arr2.length) return false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) return false;
+  }
+  return true;
+}
 
+export function largest(arr) {
+  return Math.max(...arr);
+}
+
+export function zeroest(arr) {
+  current = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < current)
+      current = arr[i];
+    }
+    return current;
 }
